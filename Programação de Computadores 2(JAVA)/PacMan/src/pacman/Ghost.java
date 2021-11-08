5574b5a87054f95b2959ba979b1e1d100cf55dd2
GiovanneMika <69139838+GiovanneMika@users.noreply.github.com>
2021-11-08 13:59:58 -0300

    inicio da copia do pacman

---
+++ Programação de Computadores 2(JAVA)/PacMan/src/pacman/Ghost.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacman;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author 2info2021
+ */
+public class Ghost extends Thread {
+
+    private int num = 1;
+    private Image imagem = new ImageIcon(getClass().getResource("/imagens/ghost_1_0.png")).getImage().getScaledInstance(20, 20, 0);
+    private int status = 0;
+    private int dx = 0;
+    private int dy = 0;
+    private int x = 9;
+    private int y = 10;
+    private int altura = 20;
+    private int largura = 20;
+    int seq = 0;
+
+    /**
+     * @return the num
+     */
+    public int getNum() {
+        return num;
+    }
+
+    /**
+     * @param num the num to set
+     */
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    /**
+     * @return the imagem
+     */
+    public Image getImagem() {
+        return imagem;
+    }
+
+    /**
+     * @param imagem the imagem to set
+     */
+    public void setImagem(Image imagem) {
+        this.imagem = imagem;
+    }
+
+    /**
+     * @return the status
+     */
+    public int getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the dx
+     */
+    public int getDx() {
+        return dx;
+    }
+
+    /**
+     * @param dx the dx to set
+     */
+    public void setDx(int dx) {
+        this.dx = dx;
+    }
+
+    /**
+     * @return the dy
+     */
+    public int getDy() {
+        return dy;
+    }
+
+    /**
+     * @param dy the dy to set
+     */
+    public void setDy(int dy) {
+        this.dy = dy;
+    }
+
+    /**
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @return the y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     * @return the altura
+     */
+    public int getAltura() {
+        return altura;
+    }
+
+    /**
+     * @param altura the altura to set
+     */
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    /**
+     * @return the largura
+     */
+    public int getLargura() {
+        return largura;
+    }
+
+    /**
+     * @param largura the largura to set
+     */
+    public void setLargura(int largura) {
+        this.largura = largura;
+    }
+
+}
