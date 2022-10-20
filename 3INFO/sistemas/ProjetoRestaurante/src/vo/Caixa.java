0b981b6c478c7e3baddf2a093ab9619fd7d03089
GiovanneMika <riskydiamond385@gmail.com>
2022-10-20 11:05:47 -0300

    Update Caixa.java

--- 3INFO/sistemas/ProjetoRestaurante/src/vo/Caixa.java
+++ 3INFO/sistemas/ProjetoRestaurante/src/vo/Caixa.java
@@ -5,14 +5,27 @@
  */
 package vo;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 /**
  *
  * @author 2info2021
  */
 @Entity
-public class Caixa {
+public class Caixa implements Serializable {
+    
+    @Id
     private int id = 0;
     private int idCaixa = 1;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
